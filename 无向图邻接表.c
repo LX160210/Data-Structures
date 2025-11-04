a89a857ff6ae8904f1a3cb53f4354e5a9dd2f54d
Liu Xiang <2024214618@stu.cqupt.edu.cn>
2025-11-04 12:06:52 +0800

    Correct vertex initialization in InitVertice function
    Fix initialization of vertices in the graph.

---
+++ 无向图邻接表.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+define MAXSIZE 100
+
+typedef struct AMGraph{
+Vertice V[MAXSIZE];
+Adjvex A[MAXSIZE];
+} AMGraph;
+
+typedef struct Vertice{
+char v[];
+} Vertice;
+
+typedef struct Adjvex{
+int data;
+Adjvex *next;
+} Adjvex;
+
+void InitVertice(AMGraph *G,int n){
+for(int i=1;i<n;i++){
+G->V[i].v[0]='V'；
+G->V[i].v[1]='i'；
+G->V[i].v[2]='\0'；
+}
+}
+
+void InitAdjvex(AMGraph *G,int n){
+  
+}
