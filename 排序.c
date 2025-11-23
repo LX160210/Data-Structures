f6f4d9fc35ad40e90a79194d9ed3602513e5e30f
Liu Xiang <2580617135@qq.com>
2025-11-23 18:26:19 +0800

    Add files via upload

---
+++ 排序.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int data01[] = {0, 49, 38, 65, 97, 76, 13, 27, 49};
+int data02[] = {0, 49, 38, 65, 97, 76, 13, 27, 49, 55, 4};
+int data1[] = {0, 49, 38, 65, 97, 76, 13, 27, 49};
+int data2[] = {0, 49, 38, 65, 97, 76, 13, 27, 49, 55, 4};
+int length1 = sizeof(data1) / sizeof(data1[0]) - 1;
+int length2 = sizeof(data2) / sizeof(data2[0]) - 1;
+
+void resetdata(int *data, int *source, int length)
+{
+    for (int i = 0; i < length + 1; i++)
+    {
+        data[i] = source[i];
+    }
+}
+
+void InsertSort(int *data, int length)
+{
+    int current = 2;
+    for (int i = 2; i < length + 1; i++)
+    {
+        if (data[current] < data[current - 1])
+        {
+            data[0] = data[current];
+            data[current] = data[current - 1];
+            int pos = current - 2;
+            while (pos > 0 && data[0] < data[pos])
+            {
+                data[pos + 1] = data[pos];
+                pos = pos - 1;
+            }
+            data[pos + 1] = data[0];
+        }
+        current++;
+    }
+    for (int i = 1; i < length + 1; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    resetdata(data, data01, length1);
+}
+
+void BInertSort(int *data, int length)
+{
+    int current = 2;
+    for (int i = 2; i < length + 1; i++)
+    {
+        if (data[current] < data[current - 1])
+        {
+            data[0] = data[current];
+            int left = 1, right = current - 1;
+            int mid;
+            while (right >= left)
+            {
+                mid = (left + right) / 2;
+                if (data[0] > data[mid])
+                {
+                    left = mid + 1;
+                }
+                else if (data[0] < data[mid])
+                {
+                    right = mid - 1;
+                }
+                else if (data[0] == data[mid])
+                {
+                    mid++;
+                    break;
+                }
+            }
+            for (int j = current - 1; j >= mid; j--)
+            {
+                data[j + 1] = data[j];
+            }
+            data[mid] = data[0];
+        }
+        current++;
+    }
+    for (int i = 1; i < length + 1; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    resetdata(data, data01, length1);
+}
+
+void ShellInsertSort(int *data, int length)
+{
+    for (int gap = length / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap + 1; i < length + 1; i++)
+        {
+            if (data[i] < data[i - gap])
+            {
+                data[0] = data[i];
+                data[i] = data[i - gap];
+                int pos = i - gap - gap;
+                while (pos > 0 && data[0] < data[pos])
+                {
+                    data[pos + gap] = data[pos];
+                    pos = pos - gap;
+                }
+                data[pos + gap] = data[0];
+            }
+        }
+    }
+    for (int i = 1; i < length + 1; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    resetdata(data, data02, length2);
+}
+
+void BubbleSort(int *data, int length)
+{
+    for (int i = 1; i < length + 1; i++)
+    {
+        int k = 0;
+        for (int j = 2, temp; j < length + 1 - i; j++)
+        {
+            if (data[j] < data[j - 1])
+            {
+                temp = data[j];
+                data[j] = data[j - 1];
+                data[j - 1] = temp;
+                k = 1;
+            }
+        }
+        if (k == 0)
+        {
+            break;
+        }
+    }
+    for (int i = 1; i < length + 1; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    resetdata(data, data01, length1);
+}
+
+// int data1[] = {0, 49, 38, 65, 97, 76, 13, 27, 49};
+void quicksort(int *data, int left, int right)
+{
+    if (left >= right)
+    {
+        return;
+    }
+    int pivot = data[(left + right) / 2];
+    int begin = left, end = right;
+    while (begin <= end)
+    {
+        while (begin <= end && data[begin] < pivot)
+        {
+            begin++;
+        }
+        while (end >= begin && data[end] > pivot)
+        {
+            end--;
+        }
+        if (begin <= end)
+        {
+            data[0] = data[begin];
+            data[begin] = data[end];
+            data[end] = data[0];
+            begin++;
+            end--;
+        }
+    }
+    quicksort(data, left, end);
+    quicksort(data, begin, right);
+}
+
+void QuickSort(int *data, int left, int right, int length)
+{
+    quicksort(data, left, right);
+    for (int i = 1; i < length + 1; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    resetdata(data, data01, length1);
+}
+int main()
+{
+    InsertSort(data1, length1);
+    BInertSort(data1, length1);
+    ShellInsertSort(data2, length2);
+    BubbleSort(data1, length1);
+    QuickSort(data1, 1, length1, length1);
+    return 0;
+}
